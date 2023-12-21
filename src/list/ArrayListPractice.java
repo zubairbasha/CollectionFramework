7e76fa9a087211109ba3125e37f427a0095d9446
zubair basha <mohammedzubair006@gmail.com>
2023-12-21 22:50:49 +0530

    Initial Commit

---
+++ src/list/ArrayListPractice.java
@@ -0,0 +1,15 @@
+package list;
+
+import java.util.ArrayList;
+
+public class ArrayListPractice {
+	public static void main(String[] args) {
+		ArrayList<String> words=new ArrayList<> ();
+		ArrayList words2=new ArrayList();
+		words.add("Zubair");
+		words.add("Basha");
+		words.add("Ashik");
+		//To get the Element we use get method
+		System.out.println(words.get(0));
+	}
+}
